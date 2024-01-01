e5bb072c7bfd6c1321bf956684892047f2deefdf
punithguptha <sai@Sais-MacBook-Air.local>
2024-01-02 00:11:45 +0530

    Adding express-validator and some validations using it

--- index.js
+++ index.js
@@ -1,26 +1,23 @@
 //This is the entry point of our application
 
-const connectToMongo=require('./db');
-const express=require('express');
+const connectToMongo = require("./db");
+const express = require("express");
 
 connectToMongo();
-const app =express();
-const port=3000;
+const app = express();
+const port = 3000;
 
 //Middleware to accept and return json content in a request
 app.use(express.json());
 
-app.get('/',(req,res)=>{
-    res.send("Hello Daisy!");
-})
+app.get("/", (req, res) => {
+  res.send("Hello Daisy!");
+});
 
 //Exposing the available routes
-app.use('/api/auth',require('./routes/auth'));
-app.use('/api/notes',require('./routes/notes'));
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/notes", require("./routes/notes"));
 
-
-app.listen(port,()=>{
-    console.log(`Example application listening at http://localhost:${port}`)
-})
-
-
+app.listen(port, () => {
+  console.log(`Example application listening at http://localhost:${port}`);
+});
--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,20 @@
-const express=require('express');
-const User = require('../models/User');
-const router=express.Router();
+const express = require("express");
+const User = require("../models/User");
+const router = express.Router();
+const {body,validationResult}=require('express-validator');
 
-router.post('/',(req,res)=>{
-    console.log(req.body);
-    let user=new User(req.body);
-    user.save(); //Saves to mongodb
+
+
+router.post("/",[body('email',"Enter a valid Email").isEmail(),body('name',"Min length of name required is 3").isLength({min:3}),body('password',"Password should atleast be 5 characters long").isLength({min:5})], (req, res) => {
+  const valResult=validationResult(req);
+  if(!valResult.isEmpty()){
+    return res.status(400).json({'errors':valResult.array()})
+  }
+  let user = new User(req.body);
+//  user.save();
+  user.save().then(user=>res.json(user)).catch(err => {
+    res.json({'error':"Duplicate email entry detected!!",'message':err.message});
+  });
 });
 
-module.exports=router;
+module.exports = router;
