0e6f24291459d096720585a23cf3d7748339eb1c
punithguptha <sai@Sais-MacBook-Air.local>
2024-01-04 00:08:23 +0530

    Adding authtoken support and hashing password with jwt and bcryptjs WIP

--- routes/auth.js
+++ routes/auth.js
@@ -2,26 +2,32 @@ const express = require("express");
 const User = require("../models/User");
 const router = express.Router();
 const { body, validationResult } = require("express-validator");
+const bcrypt = require("bcryptjs");
+const jwt=require('jsonwebtoken');
 
-// POST: https://localhost:3000/api/auth/createuser.
-// Desc: Creates a user with the provided details
-router.post("/createuser", [body("email", "Enter a valid Email").isEmail(), body("name", "Min length of name required is 3").isLength({ min: 3 }), body("password", "Password should atleast be 5 characters long").isLength({ min: 5 })], 
-async (req, res) => {
+/* 
+    POST: https://localhost:3000/api/auth/createuser.
+    Description: Creates a user with the provided details
+*/
+router.post("/createuser", [body("email", "Enter a valid Email").isEmail(), body("name", "Min length of name required is 3").isLength({ min: 3 }), body("password", "Password should atleast be 5 characters long").isLength({ min: 5 })], async (req, res) => {
   const valResult = validationResult(req);
   if (!valResult.isEmpty()) {
     return res.status(400).json({ errors: valResult.array() });
   }
   //Check whether user with same email exists already
   try {
-    let userExists=await User.findOne({"email":req.body.email});
-    if(userExists){
-      return res.status(400).json({"error":"User with same email exists already!"});
+    let userExists = await User.findOne({ email: req.body.email });
+    if (userExists) {
+      return res.status(400).json({ error: "User with same email exists already!" });
     }
-    let user = new User(req.body);
+    let safePassword = await bcrypt.hash(req.body.password, 10);
+    let user = new User({ name: req.body.name, email: req.body.email, password: safePassword });
     await user.save();
-    return res.json({"User":user});
+    const privateKey=process.env.PRIVATE_KEY?process.env.PRIVATE_KEY:"defaultprivate@key";
+    const authToken=jwt.sign( {user:{id: user.id }},privateKey);
+    return res.json({"authToken":authToken});
   } catch (error) {
-   res.status(500).json({"error":"Some error occured"}); 
+    res.status(500).json({ error: "Some error occured" });
   }
 });
 
